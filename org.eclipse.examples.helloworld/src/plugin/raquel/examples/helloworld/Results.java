77fe1bfafb47681dbc6504d3ca0f6de07db5a6e6
raquelbromao <raquelbromao@gmail.com>
2016-08-24 16:57:39 -0300

    Nova janela funcionando já c/ o alg de Message Chain

--- org.eclipse.examples.helloworld/src/plugin/raquel/examples/helloworld/Results.java
+++ org.eclipse.examples.helloworld/src/plugin/raquel/examples/helloworld/Results.java
@@ -1,13 +1,22 @@
 package plugin.raquel.examples.helloworld;
 
+import java.util.regex.Pattern;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
 //import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.action.StatusLineManager;
 import org.eclipse.jface.window.ApplicationWindow;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -19,12 +28,75 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Text;
 
 public class Results extends ApplicationWindow {
-	private Text text;
+	private static Text results;
 	static IPackageFragment[] packagesSelection;
 
 	public static void Inicializa(IPackageFragment[] p) {
 		packagesSelection = p;
 	}
+	
+	public static void splitMessageChain(String s) {
+		// retira o ";" do final da string
+		s = s.replace(";", " ");
+
+		// Quebra a variável quando acha . e armazena a sobra numa posição do
+		// array aux
+		// a().b() -> . é descartando e a() fica em aux[0] e b() em aux[1]
+		String[] aux = s.split(Pattern.quote("."));
+
+		// Pega o tamanho da string aux
+		// Imprime a variável aux na tela
+		results.append("Objeto: " + aux[0] + "\n");
+		for (int i = 1; i < aux.length; i++) {
+			results.append("Método[" + i + "]: " + aux[i] + "\n");
+		}
+
+		results.append("_______________________________________________________\n");
+	}
+
+	public static void verificaMessageChain(String s) {
+		// verifica se a expressão coletada é igual ao regex criado
+		// não foi usado [;] no final do regex pq o compilador nem lê se não
+		// houver ele no final
+		if (s.matches("[\\w]+([\\.]+[\\w]+[(]+[)]){2,}")) { // "[\\w]+([\\.]+[\\w]+[(]+[?\\w]+[)]){2,}")
+			results.append("\nMessage Chain: " + s + "\n");
+			splitMessageChain(s);
+		} else {
+			results.append("\nNão é Message Chain: " + s + "\n_______________________________________________________\n");
+		}
+	}
+
+	private void analyseClass(ICompilationUnit classe) throws JavaModelException {
+		// ICompilationUnit unit = classe;
+		// now create the AST for the ICompilationUnits
+		CompilationUnit parse = parse(classe);
+		ExpressionInvoke visitor = new ExpressionInvoke();
+		parse.accept(visitor);
+
+		// Imprime na tela o nome do método e o tipo de retorno
+		for (ExpressionStatement method : visitor.getMethods()) {
+			String t = null;
+			t = method.getExpression().toString();
+
+			verificaMessageChain(t);
+		}
+	}
+
+	/**
+	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+	 * Java source file
+	 *
+	 * @param unit
+	 * @return
+	 */
+	private static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null); // parse
+	}
+
 
 	/**
 	 * Create the application window,
@@ -81,8 +153,8 @@ public class Results extends ApplicationWindow {
 		btnApplyClass.setBounds(500, 7, 75, 25);
 		btnApplyClass.setText("Apply");
 
-		text = new Text(container, SWT.BORDER);
-		text.setBounds(10, 49, 484, 334);
+		results = new Text(container, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		results.setBounds(10, 49, 484, 334);
 
 		Button btnCancel = new Button(container, SWT.NONE);
 		btnCancel.setText("Cancel");
@@ -91,6 +163,42 @@ public class Results extends ApplicationWindow {
 		Button btnClear = new Button(container, SWT.NONE);
 		btnClear.setText("Clear");
 		btnClear.setBounds(500, 49, 75, 25);
+		
+		btnApplyClass.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				try {
+					// LIMPA A JANELA DOS RESULTADOS QUANDO SELECIONADO UMA NOVA CLASSE
+					results.setText("");
+					String nameClass = comboClasses.getItem(comboClasses.getSelectionIndex());
+
+					for (IPackageFragment mypackage : packagesSelection) {
+						for (final ICompilationUnit compilationUnit : mypackage.getCompilationUnits()) {
+							String aux = compilationUnit.getElementName();
+							if (aux.equals(nameClass)) {
+								analyseClass(compilationUnit);
+							}
+						}
+					}
+
+				} catch (JavaModelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		btnCancel.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				close();
+			}
+		});
+
+		btnClear.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				results.setText("");
+			}
+		});
 
 		return container;
 	}
